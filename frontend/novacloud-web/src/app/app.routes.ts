53f229f39a128f638c99296bd5e79f6d0a205d08
Elteclass <l22211519@tijuana.tecnm.mx>
2026-04-03 15:12:27 -0700

    feat(auth): maquetacion de interfaz de login y enrutamiento lazy loading

--- frontend/novacloud-web/src/app/app.routes.ts
+++ frontend/novacloud-web/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadChildren: () => import('./features/auth/auth-module').then(m => m.AuthModule)
+  }
+];
