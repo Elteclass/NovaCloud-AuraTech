bd22c646456bb0d8aadb1e0c46c0a77150a12a22
Elteclass <L22211519@tijuana.tecnm.mx>
2026-04-26 17:27:28 -0700

    feat(layout): integrar modal global de vista previa en main layout

--- frontend/novacloud-web/src/app/features/dashboard/main-layout/main-layout.ts
+++ frontend/novacloud-web/src/app/features/dashboard/main-layout/main-layout.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Sidebar } from '../components/sidebar/sidebar';
 import { Topbar } from '../components/topbar/topbar';
+import { FilePreviewModalComponent } from '../../../shared/components/file-preview-modal/file-preview-modal.component';
+import { FilePreviewService } from '../../../core/services/file-preview.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule, Sidebar, Topbar],
+  standalone: true,
+  imports: [CommonModule, RouterModule, Sidebar, Topbar, FilePreviewModalComponent],
   templateUrl: './main-layout.html'
 })
-export class MainLayout {} // O MainLayoutComponent, según como lo haya generado tu CLI
+export class MainLayout {
+  constructor(public filePreviewService: FilePreviewService) {}
+}
