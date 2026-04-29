b11e774a1774bd65def17ba323c32928685af43a
DRB03 <L22211650@TIJUANA.EDU.MX>
2026-04-28 18:11:00 -0700

    feat(dashboard): add UI triggers for folder modal and context menu

--- frontend/novacloud-web/src/app/features/dashboard/components/sidebar/sidebar.ts
+++ frontend/novacloud-web/src/app/features/dashboard/components/sidebar/sidebar.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'; // <-- 1. Importamos el enrutador
+import { RouterModule } from '@angular/router';
 import { NavItem, StorageInfo } from '../../../../core/models/cloud-file.model';
+import { FolderModalService } from '../../../../core/services/folder-modal'; 
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule, RouterModule], // <-- 2. Lo agregamos aquí
+  imports: [CommonModule, RouterModule], 
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  // Inyectamos el servicio para poder usarlo en este componente
+  folderModalService = inject(FolderModalService);
 
   // REGLA DE NEGOCIO: "Compartidos conmigo" eliminado del menú
   navItems: NavItem[] = [
@@ -24,4 +27,9 @@ export class Sidebar {
     totalGB: 50,
     percentage: 85,
   };
+
+  // Método que será llamado cuando se haga clic en el botón "Nueva Carpeta"
+  openNewFolderModal() {
+    this.folderModalService.openModal();
+  }
 }
