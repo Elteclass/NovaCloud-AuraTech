54c18beff3b73e500af62832f7e086ee59851678
DRB03 <L22211650@TIJUANA.EDU.MX>
2026-05-07 18:24:23 -0700

    fix(auth): solucionar error de ssr con localstorage

--- frontend/novacloud-web/src/app/core/guards/auth-guard.ts
+++ frontend/novacloud-web/src/app/core/guards/auth-guard.ts
@@ -1,16 +1,26 @@
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Router, CanActivateFn } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  
-  // Buscamos nuestra llave simulada en el navegador
-  const token = localStorage.getItem('auratech_token');
-  
-  if (token) {
-    return true; // Acceso concedido
-  } else {
-    router.navigate(['/login']); // Intruso detectado, de vuelta al login
-    return false;
+  const platformId = inject(PLATFORM_ID); // Inyectamos el contexto de la plataforma
+
+  // Verificamos si el código se está ejecutando en un navegador real
+  if (isPlatformBrowser(platformId)) {
+    
+    // Aquí adentro es 100% seguro usar localStorage
+    const token = localStorage.getItem('auratech_token');
+    
+    if (token) {
+      return true; // Acceso concedido
+    } else {
+      router.navigate(['/login']); // Intruso detectado, de vuelta al login
+      return false;
+    }
   }
+
+  // Si se está ejecutando en el servidor (SSR), denegamos el acceso 
+  // temporalmente hasta que el navegador cargue por completo.
+  return true;
 };
