9bc3c0c3d7336750b7d390511bb1c9cc732719d4
AaronCasildo <aacasildor@gmail.com>
2026-05-13 22:52:50 -0700

    fix(auth): agregar access token en dto de login

--- backend-core/NovaCloud.BackendCore/DTOs/Auth/LoginResponse.cs
+++ backend-core/NovaCloud.BackendCore/DTOs/Auth/LoginResponse.cs
@@ -3,6 +3,7 @@ namespace NovaCloud.BackendCore.DTOs.Auth;
 public sealed class LoginResponse
 {
     public string? Token { get; init; }
+    public string? AccessToken { get; init; }
     public string? RefreshToken { get; init; }
     public string? Role { get; init; }
     public string? Email { get; init; }
